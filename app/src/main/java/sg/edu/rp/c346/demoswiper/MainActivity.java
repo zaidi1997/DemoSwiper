d0f4ed3d2828d866821de4eb159ff5198176d24f
zaidi1997 <17053109@myrp.edu.sg>
2019-07-16 10:47:52 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoswiper/MainActivity.java
@@ -0,0 +1,64 @@
+package sg.edu.rp.c346.demoswiper;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList <Fragment> al;
+    MyFragmentPagerAdapter adapter;
+    ViewPager vPager;
+
+    Button btnBack, btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        vPager = findViewById(R.id.viewpager1);
+
+        btnBack = findViewById(R.id.btnBack);
+        btnNext = findViewById(R.id.btnNext);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new Frag1());
+        al.add(new Frag2());
+
+        adapter = new MyFragmentPagerAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (vPager.getCurrentItem() > 0){
+                    int previousPage = vPager.getCurrentItem() - 1;
+                    vPager.setCurrentItem(previousPage, true);
+                }
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int max = vPager.getChildCount();
+                if (vPager.getCurrentItem() < max-1){
+                    int nextPage = vPager.getCurrentItem() + 1;
+                    vPager.setCurrentItem(nextPage, true);
+                }
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/sg/edu/rp/c346/demoswiper/MyFragmentPagerAdapter.java
@@ -0,0 +1,26 @@
+package sg.edu.rp.c346.demoswiper;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import java.util.ArrayList;
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    ArrayList<Fragment> fragments;
+
+    public MyFragmentPagerAdapter(FragmentManager fm, ArrayList<Fragment> al) {
+        super(fm);
+        fragments = al;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
